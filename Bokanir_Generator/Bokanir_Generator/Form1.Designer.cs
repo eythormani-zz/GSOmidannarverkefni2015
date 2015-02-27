db47c3764167f4faa9295c4e01f60410c7ff8d51
Páll Guðbrandsson <gudbrandsson74@gmail.com>
2015-02-27 10:51:56 +0000

    Generatrar

---
+++ Bokanir_Generator/Bokanir_Generator/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Bokanir_Generator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Keyra";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(0, 107);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(283, 142);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fjöldi pantana";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Á hvaða hóteli ertu?";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1. Hótel Stykkishólmur",
+            "2. Hótel Ísafjörður",
+            "3. Hótel Blöndós",
+            "4. Hótel Sauðárkrókur",
+            "5. Hótel Akureyri",
+            "6. Hótel Húsavík",
+            "7. Hótel Egillsstaðir",
+            "8. Hótel Selfoss",
+            "9. Hótel Vestmaneyjar",
+            "10. Hótel Reykjavík"});
+            this.comboBox1.Location = new System.Drawing.Point(120, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Herbergi_generator/Herbergi_generator/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Herbergi_generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int teljari = 0, hotelID = 0;
+            string[] herbergi = new string[1000];
+            for (int i = 0; i < herbergi.Length; i++)
+            {
+                herbergi[i] += "(";
+                if (teljari<=4)
+                {
+                    herbergi[i] += "1," + hotelID.ToString() + "),\n";
+                }
+                else if (teljari>=5 && teljari<=7)
+                {
+                    herbergi[i] += "2," + hotelID.ToString() + "),\n";
+                }
+                else if (teljari>=8 && teljari<=58)
+                {
+                    herbergi[i] += "3," + hotelID.ToString() + "),\n";
+                }
+                else if (teljari>=59 && teljari<=60)
+                {
+                    herbergi[i] += "4," + hotelID.ToString() + "),\n";
+                }
+                else if (teljari>=61 && teljari<=63)
+                {
+                    herbergi[i] += "5," + hotelID.ToString() + "),\n";
+                }
+                else
+                {
+                    herbergi[i] += "6," + hotelID.ToString() +"),\n";
+                }
+                if (teljari==100)
+                {
+                    teljari = 0;
+                    hotelID++;
+                }
+                teljari++;
+            }//lokar for
+            for (int i = 0; i < herbergi.Length; i++)//skrifar allt út
+            {
+                richTextBox1.Text += herbergi[i];
+            }
+        }
+    }
+}
