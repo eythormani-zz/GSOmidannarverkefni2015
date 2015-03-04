40603064aedeec8f3c9c268b6a29a82d7d7fc9c3
Eyþór Máni Steinarsson <eythormani@gmail.com>
2015-03-04 11:15:30 +0000

    Bætti við sviga sem vantaði
    Vandamálið olli því að SQL spurningin keyrði ekki

--- Vefviðmót/bokun.php
+++ Vefviðmót/bokun.php
@@ -43,7 +43,7 @@ if (isset($_GET['visitors'])) {
 	AND herbergi.ID NOT IN(
 	SELECT herbergiID FROM bokanir
 	WHERE hallo <= '$komutimi' AND bless >= '$brottfarartimi'
-	";
+	)";
 
 	$logon = $dbconnect->prepare($sql);
 
